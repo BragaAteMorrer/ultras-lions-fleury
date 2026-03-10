a7f68c9055edff264278e6d3d1dea251fc8b33a1
ALMEIDA Nelson <nalmeida@unedic.fr>
2026-03-10 17:27:11 +0100

    Initial commit

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+APP_DIR="${APP_DIR:-/var/www/html}"
+VAR_DIR="$APP_DIR/var"
+UPLOAD_DIR="$APP_DIR/public/uploads"
+
+if [ -d "$VAR_DIR" ]; then
+  mkdir -p "$VAR_DIR/cache" "$VAR_DIR/log"
+  chown -R www-data:www-data "$VAR_DIR/cache" "$VAR_DIR/log"
+  chmod -R ug+rwX "$VAR_DIR/cache" "$VAR_DIR/log"
+fi
+
+mkdir -p "$UPLOAD_DIR/media" "$UPLOAD_DIR/profils"
+chown -R www-data:www-data "$UPLOAD_DIR"
+chmod -R ug+rwX "$UPLOAD_DIR"
+
+exec docker-php-entrypoint "$@"
